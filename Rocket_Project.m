51a084ff3da055c99bbefcddf0df433ef329f654
Nick <nicholas.canovas@gmail.com>
2024-10-24 19:57:47 -0400

    Add files via upload
    Second iteration of code with F v.s t implemented

--- Rocket_Project.m
+++ Rocket_Project.m
@@ -9,32 +9,43 @@ const.rho = 1.225; % density of air [kg/m^3]
 const.A = 1; % Surface Area of the Rocket [m^2]
 const.vw = 10; % Wind Velocity [m/s]
 const.dt = .01; % Time step [s]
-const.m = 10;
+const.mp_i = 0.0393; % kg
+m_prev = m_initial;
+
+
+% Here is where thrust v. time is input
+F = readmatrix('Thrust.txt');
+t_b = readmatrix('Time.txt');
+m_dot = const.mp_i/t_b(end); % ASSUMING constant m_dot (!!)
 
 % Define the states
-x = zeros(1,300); % X-direction [m]
-z = zeros(1,300); % Altitude [m]
-v = zeros(1,300); % Velocity [m/s]
-theta = zeros(1,300); % Flight Path Angle [degrees]
-psi = zeros(1,300); % Pitch Angle [degrees]
+x = zeros(300); % X-direction [m]
+z = zeros(300); % Altitude [m]
+v = zeros(300); % Velocity [m/s]
+theta = zeros(300); % Flight Path Angle [degrees]
+psi = zeros(300); % Pitch Angle [degrees]
 psi(1) = pi/4;
 theta(1) = pi/4;
 
 % Define the forces
-F = 10*ones(1,300); % Thrust [N]
+%F = 10*ones(1,300); % Thrust [N]
 L = zeros(1,300); % Lift [N]
 D = zeros(1,300); % Drag [N]
+
+% Define other changing values
+m = zeros(1,300); % mass, [lbm]
 
 %% Calculations
 i = 1;
 while (z(i) > 0) || (i == 1)
-    dt = const.dt;
     v_rel(i) = sqrt((v(i)*sind(theta(i)))^2+(const.vw + v(i)*cosd(theta(i)))^2);
 
     L(i) = lift_function(const, v_rel(i));
     D(i) = drag_function(const, v_rel(i));
 
-    [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),v(i),theta(i),psi(i),z(i));
+    m(i) = m_prev - mdot * dt;
+
+    [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),v(i),theta(i),psi(i),z(i), m(i));
 
     v(i+1) = v(i) + dt/2 * (G(i) + G_star(i));
     theta(i+1) = theta(i) + dt/2 * (H(i) + H_star(i));
@@ -42,6 +53,7 @@ while (z(i) > 0) || (i == 1)
     z(i+1) = z(i) + dt/2 * (v(i)*sind(theta(i)) + v(i+1) * sind(theta(i+1)));
 
     i = i + 1;
+    m_prev = m(i);
 end
 
 
@@ -62,9 +74,8 @@ function[D] = drag_function(const, v)
 	D = (1/2)*rho*A*v^2*c_d;
 end
 
-function[H,G,H_star, G_star] = h_g_function(const, F,L,D,v,theta,psi,z)
+function[H,G,H_star, G_star] = h_g_function(const, F,L,D,v,theta,psi,z, m)
 	g  = const.g;
-    m = const.m;
     dt = const.dt;
     vw = const.vw;
 
