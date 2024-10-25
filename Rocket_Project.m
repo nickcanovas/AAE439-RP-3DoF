9be4ea3ecc5699718976e639fcd142c33a60d023
Nick <nicholas.canovas@gmail.com>
2024-10-25 10:58:31 -0400

    Add files via upload
    Code changes made morning of 10/25. Added if statements for the different stages of flight.

--- Rocket_Project.m
+++ Rocket_Project.m
@@ -54,8 +54,7 @@ z(1) = 1;
 t(1) = 0;
 theta = zeros(1, length(F)); % Flight Path Angle [degrees]
 psi = zeros(1, length(F)); % Pitch Angle [degrees]
-psi(1) = 90; % [deg]
-theta(1) = 90.001; % [deg]
+theta(1) = 40.00; % launch angle [deg]
 
 % Define the forces
 %F = 10*ones(1,300); % Thrust [N]
@@ -66,63 +65,146 @@ D = zeros(1, length(F)); % Drag [N]
 m = zeros(1, length(F)); % mass, [kg] 
 v_rel = zeros(1, length(F));
 
+% Rod Characteristics
+L_rod = 1.8288; [m]
+CD_pc = 1.3;        % FIX THIS
+
 %% Calculations
 i = 1;
 while (z(i) > 0)
-    v_rel(i) = sqrt((v(i)*sind(theta(i)))^2+(const.vw + v(i)*cosd(theta(i)))^2);
-    psi(i) = acosd((v(i)*sind(theta(i)))/v_rel(i));
-
-    L(i) = lift_function(const, v_rel(i));
-    [CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v_rel(i), const);
-    D(i) = drag_function(const, v_rel(i),CD0_FB);
-    % L(i) = lift_function(const, v_rel(i), CD_b, CD0_F); 
-    
-    disp(L(i));
-
-    if i > 2711
-        F(i) = 0;
-        t_b(i) = 0;
-        m(i) = m_prev;
-    else
-        m(i) = m_prev - mdot * dt;
-    end
-    
-    fprintf('The cd0 is: %.2f\n', CD0_FB);
-    fprintf('The lift is: %.2f\n', L(i));
-    fprintf('The Drag is: %.2f\n', D(i));
-    fprintf('The mass is: %.2f\n', m(i));
-    %fprintf('The Thrust is: %.2f\n', F(i));
-    fprintf('The velocity is: %.2f\n', v(i));
-
-    %[H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB,CD_b,CD0_F, v(i),theta(i),psi(i),z(i), m(i));
-    [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB, v(i),theta(i),psi(i),z(i), m(i));
-    
-    fprintf('The H is: %.2f\n', H(i));
-    fprintf('The G is: %.2f\n', G(i));
-    fprintf('The H_star is: %.2f\n', H_star(i));
-    fprintf('The G_star is: %.2f\n', G_star(i));
-
-    v(i+1) = v(i) + dt/2 * (G(i) + G_star(i));
-    theta(i+1) = theta(i) + dt/2 * (H(i) + H_star(i));
-    x(i+1) = x(i) + dt/2 * (v(i)*cosd(theta(i)) + v(i+1)*cosd(theta(i+1)));
-    z(i+1) = z(i) + dt/2 * (v(i)*sind(theta(i)) + v(i+1) * sind(theta(i+1)));
-    
-    m_prev = m(i);
     t(i+1) = t(i) + const.dt;
+    if (sqrt(z(i)^2+x(i)^2)) < L_rod
+        disp('on rod.');
+        v_rel(i) = sqrt((v(i)*sind(theta(i)))^2+(const.vw + v(i)*cosd(theta(i)))^2);
+        psi(i) = acosd((v(i)*sind(theta(i)))/v_rel(i));
+
+        L(i) = lift_function(const, v_rel(i));
+        [CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v_rel(i), const);
+        D(i) = drag_function(const, v_rel(i),CD0_FB);
+        % L(i) = lift_function(const, v_rel(i), CD_b, CD0_F);
+
+        if i > 2711
+            F(i) = 0;
+            %t_b(i) = 0;
+            m(i) = m_prev;
+        else
+            m(i) = m_prev - mdot * dt;
+        end
+
+        % fprintf('The cd0 is: %.2f\n', CD0_FB);
+        % fprintf('The lift is: %.2f\n', L(i));
+        % fprintf('The Drag is: %.2f\n', D(i));
+        % fprintf('The mass is: %.2f\n', m(i));
+        % fprintf('The Thrust is: %.2f\n', F(i));
+        % fprintf('The velocity is: %.2f\n', v(i));
+
+        % [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB,CD_b,CD0_F, v(i),theta(i),psi(i),z(i), m(i));
+        [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB, v(i),theta(i),psi(i),z(i), m(i));
+
+        % fprintf('The H is: %.2f\n', H(i));
+        % fprintf('The G is: %.2f\n', G(i));
+        % fprintf('The H_star is: %.2f\n', H_star(i));
+        % fprintf('The G_star is: %.2f\n', G_star(i));
+
+        v(i+1) = v(i) + dt/2 * (G(i) + G_star(i));
+        theta(i+1) = theta(i);
+        x(i+1) = x(i) + dt/2 * (v(i)*cosd(theta(i)) + v(i+1)*cosd(theta(i+1)));
+        z(i+1) = z(i) + dt/2 * (v(i)*sind(theta(i)) + v(i+1) * sind(theta(i+1)));
+    elseif(t(i) > (t_b(end) + 6))
+        disp('parachute deployed.');
+        v_rel(i) = sqrt((v(i)*sind(theta(i)))^2+(const.vw + v(i)*cosd(theta(i)))^2);
+        psi(i) = acosd((v(i)*sind(theta(i)))/v_rel(i));
+
+        L(i) = 0;
+        %[CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v_rel(i), const);
+        % Insert parachute CD
+        D(i) = drag_function(const, v_rel(i),CD_pc);
+
+        if i > 2711
+            F(i) = 0;
+            %t_b(i) = 0;
+            m(i) = m_prev;
+        else
+            m(i) = m_prev - mdot * dt;
+        end
+        
+        % fprintf('The cd0 is: %.2f\n', CD0_FB);
+        % fprintf('The lift is: %.2f\n', L(i));
+        % fprintf('The Drag is: %.2f\n', D(i));
+        % fprintf('The mass is: %.2f\n', m(i));
+        % fprintf('The Thrust is: %.2f\n', F(i));
+        % fprintf('The velocity is: %.2f\n', v(i));
+
+        [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB, v(i),theta(i),psi(i),z(i), m(i));
+
+        % fprintf('The H is: %.2f\n', H(i));
+        % fprintf('The G is: %.2f\n', G(i));
+        % fprintf('The H_star is: %.2f\n', H_star(i));
+        % fprintf('The G_star is: %.2f\n', G_star(i));
+
+        v(i+1) = v(i) + dt/2 * (G(i) + G_star(i));
+        theta(i+1) = theta(i) + dt/2 * (H(i) + H_star(i));
+        x(i+1) = x(i) + dt/2 * (v(i)*cosd(theta(i)) + v(i+1)*cosd(theta(i+1)));
+        z(i+1) = z(i) + dt/2 * (v(i)*sind(theta(i)) + v(i+1) * sind(theta(i+1)));
+    else
+        v_rel(i) = sqrt((v(i)*sind(theta(i)))^2+(const.vw + v(i)*cosd(theta(i)))^2);
+        psi(i) = acosd((v(i)*sind(theta(i)))/v_rel(i));
+
+        L(i) = lift_function(const, v_rel(i));
+        [CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v_rel(i), const);
+        D(i) = drag_function(const, v_rel(i),CD0_FB);
+        % L(i) = lift_function(const, v_rel(i), CD_b, CD0_F);
+
+        disp('else');
+
+        if i > 2711
+            F(i) = 0;
+            %t_b(i) = 0;
+            m(i) = m_prev;
+        else
+            m(i) = m_prev - mdot * dt;
+        end
+
+        % fprintf('The cd0 is: %.2f\n', CD0_FB);
+        % fprintf('The lift is: %.2f\n', L(i));
+        % fprintf('The Drag is: %.2f\n', D(i));
+        % fprintf('The mass is: %.2f\n', m(i));
+        % fprintf('The Thrust is: %.2f\n', F(i));
+        % fprintf('The velocity is: %.2f\n', v(i));
+
+        % [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB,CD_b,CD0_F, v(i),theta(i),psi(i),z(i), m(i));
+        [H(i),G(i), H_star(i), G_star(i)] = h_g_function(const,F(i),L(i),D(i),CD0_FB, v(i),theta(i),psi(i),z(i), m(i));
+
+        % fprintf('The H is: %.2f\n', H(i));
+        % fprintf('The G is: %.2f\n', G(i));
+        % fprintf('The H_star is: %.2f\n', H_star(i));
+        % fprintf('The G_star is: %.2f\n', G_star(i));
+
+        v(i+1) = v(i) + dt/2 * (G(i) + G_star(i));
+        theta(i+1) = theta(i) + dt/2 * (H(i) + H_star(i));
+        x(i+1) = x(i) + dt/2 * (v(i)*cosd(theta(i)) + v(i+1)*cosd(theta(i+1)));
+        z(i+1) = z(i) + dt/2 * (v(i)*sind(theta(i)) + v(i+1) * sind(theta(i+1)));
+    end
+
+    m_prev = m(i);
     i = i + 1
 end
 %%
 figure(1)
 plot(x,z)
+axis("equal")
 figure(2)
 plot(t,z)
+figure(3)
+plot(t,v)
+[CD0_FB, ~, ~] = AAE439_Project_dragcoeff(v_rel(10730), const)
 
 %% Functions
 function[L] = lift_function(const, v)
     rho = const.rho;
     A = const.S_F;
 
-    c_l = 0.5; % cl(rho,v)
+    c_l = 0.3; % cl(rho,v)
 	L = (1/2)*rho*A*v^2*c_l;
 end
 
@@ -157,7 +239,7 @@ function[H,G,H_star, G_star] = h_g_function(const,F,L,D,cd, v,theta,psi,z, m)
 end
 
 function [CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v, const)
-    % v = vehicle velocity (m/s)
+    v = abs(v);% v = vehicle velocity (m/s)
     rho = const.rho; % rho = air density (kg/m^3)
 
     l_b = const.l_b; % body length (m)
@@ -205,7 +287,9 @@ function [CD0_FB, CD_b, CD0_F] = AAE439_Project_dragcoeff(v, const)
     else
         Cf_b = 0.074/(Re_l^0.2) - Re_cr*(Cf_turbulent - Cf_laminar)/Re_l + delCf_turbulent; % skin friction coefficient for body
     end
-    
+    if v < 3.1
+        Cf_b = abs(Cf_b);
+    end
     CD_f_B = Cf_b*(1 + 60/(l_b/d_m)^3 + 0.0025*(l_b/d_m))*SS_SM; % forebody drag coefficient
     CD_b = (0.029*(d_b/d_m)^3)/sqrt(CD_f_B); % base drag coefficient
     CD0_B = CD_f_B + CD_b; % zero-lift drag coefficient for body
